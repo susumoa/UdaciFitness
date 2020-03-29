33b7279584cfc8e620ea1d1c1786fc5d27e4e96f
susumoa <tobiasbori@gmail.com>
2020-03-29 14:27:56 +0100

    feat: update AddEntry, create TextButton

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 import { View, TouchableOpacity, Text } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
 import { getMetricMetaInfo, timeToString } from '../utils/helpers'
 import UdaciSlider from './UdaciSlider'
 import UdaciSteppers from './UdaciSteppers'
 import DateHeader from './DateHeader'
+import TextButton from './TextButton'
 
 function SubmitBtn ({ onPress }) {
   return (
@@ -75,8 +77,33 @@ export default class AddEntry extends Component {
     // Clear local notification
   }
 
+  reset = () => {
+    const key = timeToString()
+
+    // Update Redux
+
+    // Route to home
+
+    // Update DB
+  }
+
   render() {
     const metaInfo = getMetricMetaInfo()
+
+    if (this.props.alreadyLogged) {
+      return (
+        <View>
+          <Ionicons
+            name='md-happy'
+            size={100}
+          />
+          <Text>You already logged your information for today</Text>
+          <TextButton onPress={this.reset}>
+            Reset
+          </TextButton>
+        </View>
+      )
+    }
 
     return (
       <View>
