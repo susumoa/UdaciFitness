363979289ed243c5d9829ed2c8720f2d2e268773
susumoa <tobiasbori@gmail.com>
2020-03-30 12:37:17 +0100

    feat: add create-store

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { View } from 'react-native'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from '../reducers'
 import AddEntry from './components/AddEntry'
 
 export default function App() {
   return (
-    <View>
-      <AddEntry />
-    </View>
+    <Provider store={createStore(reducer)}>
+      <View>
+        <AddEntry />
+      </View>
+    </Provider>
+
   )
 }
