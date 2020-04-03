bcad00c2ac2cb9eae7d2ff088f568caa51f4875a
susumoa <tobiasbori@gmail.com>
2020-04-03 19:47:28 +0100

    feat: can reset Entry detail

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
 import { connect } from 'react-redux'
 import { white } from '../utils/colors'
+import { removeEntry } from '../utils/api'
+import { timeToString, getDailyReminderValue } from '../utils/helpers'
+import { addEntry } from '../actions'
 import MetricCard from './MetricCard'
+import TextButton from './TextButton'
 
 
 class EntryDetail extends Component {
+  reset = () => {
+    const { remove, entryId, navigation } = this.props
+
+    remove()
+    navigation.goBack()
+    removeEntry(entryId)
+  }
+
+  shouldComponentUpdate(nextProps) {
+    return nextProps.metrics !== null && !nextProps.metrics.today
+  }
+
   render() {
     const { metrics } = this.props
     return (
       <View>
         <View style={styles.container}>
           <MetricCard metrics={metrics} />
+          <TextButton onPress={this.reset} style={{margin: 20}}>
+            RESET
+          </TextButton>
         </View>
       </View>
     )
@@ -34,4 +54,16 @@ function mapStateToProps(entries, { route }) {
   }
 }
 
-export default connect(mapStateToProps)(EntryDetail)
+function mapDispatchToProps(dispatch, { route }) {
+  const { entryId } = route.params
+
+  return {
+    remove: () => dispatch(addEntry({
+      [entryId]: timeToString() === entryId
+        ? getDailyReminderValue()
+        : null
+    }))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(EntryDetail)
