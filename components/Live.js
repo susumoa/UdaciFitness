75f5fea1cd4dba02fe5fee979f45130b9921aee1
susumoa <tobiasbori@gmail.com>
2020-04-04 13:37:25 +0100

    feat: create undetermined status UI

--- components/Live.js
+++ components/Live.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
-import { View, Text, ActivityIndicator } from 'react-native'
+import { View, Text, ActivityIndicator, TouchableOpacity, StyleSheet } from 'react-native'
+import { Foundation } from '@expo/vector-icons'
+import { white, purple } from '../utils/colors'
 
 export default class Live extends Component {
   state = {
     coords: null,
-    status: null,
+    status: 'undetermined',
     direction: ''
   }
   render() {
@@ -24,17 +26,48 @@ export default class Live extends Component {
 
     if (status === 'undetermined') {
       return (
-        <View>
-          <Text>Undetermined</Text>
+        <View style={styles.center}>
+          <Foundation name='alert' size={60} />
+          <Text style={{textAlign: 'center', marginTop: 15}}>
+            You need to enable location services for this app.
+          </Text>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Enable</Text>
+          </TouchableOpacity>
         </View>
       )
     }
 
     return (
-      <View>
+      <View style={styles.container}>
         <Text>Live</Text>
         <Text>{JSON.stringify(this.state)}</Text>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between'
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 30,
+    marginRight: 30,
+  },
+  button: {
+    padding: 10,
+    backgroundColor: purple,
+    alignSelf: 'center',
+    borderRadius: 5,
+    margin: 20,
+  },
+  buttonText: {
+    color: white,
+    fontSize: 20,
+  }
+})
