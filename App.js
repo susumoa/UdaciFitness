e7ecdd0de958ecf6d7c13e5ea3ccbdba8ddedddc
susumoa <tobiasbori@gmail.com>
2020-04-04 13:20:00 +0100

    feat: add Live component

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { View, Platform, StatusBar } from 'react-native'
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { createStore } from 'redux'
@@ -13,6 +13,7 @@ import { purple, white } from './utils/colors'
 import AddEntry from './components/AddEntry'
 import History from './components/History'
 import EntryDetail from './components/EntryDetail'
+import Live from './components/Live'
 
 function UdaciStatusBar({ backgroundColor, ...props }) {
   return (
@@ -55,7 +56,14 @@ function Tab() {
           tabBarIcon: () => <FontAwesome name='plus-square' size={25} color={Platform.OS === 'ios' ? purple : white} />
         }}
       />
-      
+      <Tabs.Screen
+        name='Live'
+        component={Live}
+        options={{
+          tabBarLabel: 'Live',
+          tabBarIcon: () => <Ionicons name='ios-speedometer' size={25} color={Platform.OS === 'ios' ? purple : white} />
+        }}
+      />
     </Tabs.Navigator>
   )
 }
